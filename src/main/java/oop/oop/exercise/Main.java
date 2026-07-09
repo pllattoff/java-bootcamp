b7d97c4943df7052e8a05f96ac51d7391d0a8529
Stanislav Platov <pllattoff@gmail.com>
2026-07-09 16:02:43 +0200

    feat: add class Person and its objects

--- src/main/java/oop/oop/exercise/Main.java
+++ src/main/java/oop/oop/exercise/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Car car1 = new Car("BMW", "M5", "gray", 2025);
         Car car2 = new Car("Porsche", "911", "white", 2010);
 
-
         car1.start();
         car2.start();
 
@@ -21,6 +20,24 @@ public class Main {
 
         car1.stop();
         car2.stop();
+
+
+        Person person1 = new Person("Max", 31, "m");
+        Person person2 = new Person("Jack", 45, "m");
+        Person person3 = new Person("Max", 31, "m");
+
+        person1.introduce();
+        person2.introduce();
+        person3.introduce();
+
+        System.out.println("person1.toString(): " + person1);
+        System.out.println("person2.toString(): " + person2);
+        System.out.println("person3.toString(): " + person3);
+        System.out.println("person1.equals(person2): " + person1.equals(person2));
+        System.out.println("person1.equals(person3): " + person1.equals(person3));
+        System.out.println("person1.hashCode(): " + person1.hashCode());
+        System.out.println("person2.hashCode(): " + person2.hashCode());
+        System.out.println("person3.hashCode(): " + person3.hashCode());
 
     }
 }
---
+++ src/main/java/oop/oop/exercise/Person.java
@@ -0,0 +1,57 @@
+package oop.oop.exercise;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void introduce() {
+        System.out.println("Hallo, ich bin "+name+" und "+age+" Jahre alt.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+
+        return name.equals(person.name)
+                && age == person.age
+                && gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
