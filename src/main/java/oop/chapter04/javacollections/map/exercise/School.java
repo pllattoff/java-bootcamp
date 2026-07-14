7cf890461e586db1bbdb3648878b24db9a97cef8
Stanislav Platov <pllattoff@gmail.com>
2026-07-14 12:10:28 +0200

    feat: using Map instead of List

---
+++ src/main/java/oop/chapter04/javacollections/map/exercise/School.java
@@ -0,0 +1,37 @@
+package oop.chapter04.javacollections.map.exercise;
+
+import oop.chapter04.javacollections.list.exercise.Course;
+import oop.chapter04.javacollections.list.exercise.Student;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class School {
+
+    private Map<Integer, Student> students = new HashMap<>();
+
+    public void addStudent(Student student) {
+        students.put(student.getStudentId(), student);
+    }
+
+    public void printStudents() {
+        for (Map.Entry<Integer, Student> entry : students.entrySet()) {
+            System.out.println("Key (ID): " + entry.getKey() + ", Value (Student): " + entry.getValue());
+        }
+    }
+
+    public Student getStudent(int studentId) {
+        return students.get(studentId);
+    }
+
+    public void deleteStudent(int studentId) {
+        students.remove(studentId);
+    }
+
+    public List<Course> getCourses(int studentId) {
+        return students.get(studentId).getCourses();
+    }
+
+}
