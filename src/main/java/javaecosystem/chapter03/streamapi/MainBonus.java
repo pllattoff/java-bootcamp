bb6d4c47ade896df69aeea9650d25117db793061
Stanislav Platov <pllattoff@gmail.com>
2026-07-19 20:24:37 +0200

    feat: using Stream API

---
+++ src/main/java/javaecosystem/chapter03/streamapi/MainBonus.java
@@ -0,0 +1,28 @@
+package javaecosystem.chapter03.streamapi;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MainBonus {
+    static void main() throws IOException {
+
+        List<Student> strings = Files.lines(Path.of("java streams - students.csv"))
+                .peek(System.out::println)
+                .skip(1)
+                .distinct()
+                .filter(line -> !line.isEmpty())
+                .map(line -> line.split(","))
+                .map(lineAsArray -> new Student(
+                        Integer.parseInt(lineAsArray[0]),
+                        lineAsArray[1],
+                        lineAsArray[2],
+                        Integer.parseInt(lineAsArray[3])))
+                .toList();
+
+        System.out.println(strings);
+
+
+    }
+}
---
+++ src/main/java/javaecosystem/chapter03/streamapi/Student.java
@@ -0,0 +1,4 @@
+package javaecosystem.chapter03.streamapi;
+
+public record Student(Integer id, String name, String postalCode, Integer age) {
+}
