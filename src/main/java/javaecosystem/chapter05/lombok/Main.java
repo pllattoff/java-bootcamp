356db61d1aa15dad0c94d9955ac681beaf9d3962
Stanislav Platov <pllattoff@gmail.com>
2026-07-20 17:33:32 +0200

    using Lombok Data

--- src/main/java/javaecosystem/chapter05/lombok/Main.java
+++ src/main/java/javaecosystem/chapter05/lombok/Main.java
@@ -13,10 +13,18 @@ public class Main {
         Student student3 = new Student(3, "Ann", "address 3", 8);
 
         Course course1 = new Course(1, "Course Name 1", teacher1, List.of(student1, student2, student3));
-        Course course2 = new Course(2, "Course Name 2", teacher2, List.of(student1, student2, student3));
+        Course course2 = new Course(2, "Course Name 2", teacher2, List.of(student1, student3));
 
         System.out.println(course1);
         System.out.println(course2);
+
+        System.out.println("Students of the Course 1:");
+        course1.getStudents().stream()
+                .forEach(System.out::println);
+
+        System.out.println("Students of the Course 2:");
+        course2.getStudents().stream()
+                .forEach(System.out::println);
 
     }
 }
